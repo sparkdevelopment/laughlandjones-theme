e4bfcf511153b5aabe3fa763b07fd912f9947348
Ryan Jarrett <ryan@sparkdevelopment.co.uk>
2019-07-02 19:05:19 +0100

    Add CMB2 directly to theme

--- functions.php
+++ functions.php
@@ -8,8 +8,6 @@
  * across separate files in the `/src` directory.
  */
 
-require_once get_stylesheet_directory() . '/vendor/autoload.php';
-
 require_once 'src/styles.php';
 require_once 'src/scripts.php';
 
@@ -19,3 +17,9 @@ require_once 'src/supports.php';
 require_once 'src/post-types.php';
 require_once 'src/thumbnails.php';
 require_once 'src/taxonomies.php';
+
+if ( file_exists( dirname( __FILE__ ) . '/cmb2/init.php' ) ) {
+	require_once dirname( __FILE__ ) . '/cmb2/init.php';
+} elseif ( file_exists( dirname( __FILE__ ) . '/CMB2/init.php' ) ) {
+	require_once dirname( __FILE__ ) . '/CMB2/init.php';
+}
