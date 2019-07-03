3114c7eed65c786190f02f7bfc11c2d883e952f2
Ryan Jarrett <ryan@sparkdevelopment.co.uk>
2019-07-03 22:01:42 +0100

    Bootstrap CMB2

--- functions.php
+++ functions.php
@@ -8,6 +8,13 @@
  * across separate files in the `/src` directory.
  */
 
+// Bootstrap CMB2
+if ( file_exists( dirname( __FILE__ ) . '/cmb2/init.php' ) ) {
+	require_once dirname( __FILE__ ) . '/cmb2/init.php';
+} elseif ( file_exists( dirname( __FILE__ ) . '/CMB2/init.php' ) ) {
+	require_once dirname( __FILE__ ) . '/CMB2/init.php';
+}
+
 require_once 'src/styles.php';
 require_once 'src/scripts.php';
 
