0ecec1cef901ac875ec338188d6faa3b13a152e6
Ryan Jarrett <ryan@sparkdevelopment.co.uk>
2019-07-29 16:01:31 +0100

    Move scripts back to footer

--- src/scripts.php
+++ src/scripts.php
@@ -22,8 +22,8 @@ if ( ! function_exists( 'lj_register_scripts' ) ) {
 		wp_enqueue_script( 'swiper', get_template_directory_uri() . '/node_modules/swiper/dist/js/swiper.min.js', array( 'jquery' ), null, true );
 		wp_enqueue_script( 'lazysizes', get_template_directory_uri() . '/node_modules/lazysizes/lazysizes.min.js', array(), null, true );
 		wp_enqueue_script( 'lazysizes.unveilhooks', get_template_directory_uri() . '/node_modules/lazysizes/plugins/unveilhooks/ls.unveilhooks.min.js', array( 'lazysizes' ), null, true );
-		wp_enqueue_script( 'bootstrap-multiselect', get_template_directory_uri() . '/node_modules/bootstrap-multiselect/dist/js/bootstrap-multiselect.js', array( 'bootstrap','jquery' ), null, false );
-		wp_enqueue_script( 'bootstrap', get_template_directory_uri() . '/node_modules/bootstrap/dist/js/bootstrap.bundle.min.js', array( 'jquery' ), null, false );
+		wp_enqueue_script( 'bootstrap', get_template_directory_uri() . '/node_modules/bootstrap/dist/js/bootstrap.bundle.min.js', array( 'jquery' ), null, true );
+		wp_enqueue_script( 'bootstrap-multiselect', get_template_directory_uri() . '/node_modules/bootstrap-multiselect/dist/js/bootstrap-multiselect.js', array( 'bootstrap','jquery' ), null, true );
 	}
 }
 add_action( 'wp_enqueue_scripts', 'lj_register_scripts' );
